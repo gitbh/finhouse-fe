f815deefec703fd03dc10159b40b7614abb3d5e0
gitbh <bh.zmrlglu@gmail.com>
2021-12-01 09:32:23 +0000

    loading spinner added to the Login screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,17 +10,21 @@ const INITIAL_USER = {
 const NewComment = () => {
   const [user, setUser] = useState(INITIAL_USER);
   const [showWrongCredentials, setShowWrongCredentials] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const navigate = useNavigate();
 
   const handleLoginSubmit = (event, user) => {
+    setIsLoading(true);
     event.preventDefault();
     axios
       .post("https://finhouse-reporting.herokuapp.com/api/v1/user/login", user)
       .then((response) => {
         navigate("/home");
+        setIsLoading(false);
       })
       .catch((error) => {
+        setIsLoading(false);
         setShowWrongCredentials(true);
       });
   };
@@ -53,9 +57,13 @@ const NewComment = () => {
           onChange={handleOnChange}
           value={user.password}
         ></input>
-        <button className="ui blue button" type="submit">
-          Login
-        </button>
+        { isLoading ? (
+            <p><img style={{width:30, height:30}} src="https://upload.wikimedia.org/wikipedia/commons/b/b9/Youtube_loading_symbol_1_(wobbly).gif" alt='loading...' /></p>
+          ) : (
+            <button className="ui blue button" type="submit">
+              Login
+            </button>
+        )}
 
         {showWrongCredentials ? (
           <div className="ui relaxed divided list">
